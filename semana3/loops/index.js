2dd7f6b639f7ea35aba48d542f884d578e4633ed
ruth mendes <mendessruth@gmail.com>
2020-10-01 17:06:59 -0300

    semana3-aula4

---
+++ semana3/loops/index.js
@@ -0,0 +1,64 @@
+//************************* EXERCÍCIO 1 ******************************
+
+/*let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)*/
+// imprime o número 10. Ele ta percorrendo o mesmo bloco 5 vezes
+
+//*************************** EXERCÍCIO 2 ***************************
+
+/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}*/
+// a) vai ser impresso: 19,21,23,25,27,30 (em coluna)
+// b) sim. 
+/*let i = 0;  
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+    console.log('O indice é: ' + i) 
+  if (numero > 9) { 
+        console.log(numero)
+    }
+    i++ 
+}*/
+
+
+//****************************** EXERCÍCIO 3 *************************
+
+/*const lista = [100, 110 , 120, 130 , 140, 150]
+for (let numero of lista) {
+    if (numero > 90)
+        console.log(numero)
+}*/
+
+//***************************** EXERCÍCIO 3.B) **************************
+
+/*const divisaOriginal = [100,110,120,130,140,150]
+for (let i = 0; i < divisaOriginal.length; i = i + 1) {
+    console.log(divisaOriginal[i]/ 10)
+}*/
+
+//****************************** EXERCÍCIO 3.C) *************************
+
+/*let paresArray = [20,30,40,50,60 ]
+ for (let numero of paresArray){
+    if ( arrayl[i] % 2 == 0 ) 
+    paresArray.push(arrayl[i])
+    console.log(paresArray)
+ }*/
+
+ //****************************** EXERCÍCIO 3.d) *************************
+
+ const stringsArray = ["30", "40", '50', '60', '70', '80', '90']
+ let texto = ''
+for (let string of stringsArray){
+    texto = "o elemento do índex 0 é " + ''  + string
+    texto = "o elemento do índex 1 é " + ''  + string
+    console.log(texto)
+}
+
