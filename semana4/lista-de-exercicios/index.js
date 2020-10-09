9b8e87f70500658e2f43735794e495d2b6e8fcb1
ruth mendes <mendessruth@gmail.com>
2020-10-09 17:22:34 -0300

    lista-de-exercicios

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,33 @@
+/******************* Exercicio 1 (INTERPRETAÇÃO)********************/
+
+
+/* tem uma função, na qual nessa função tem uma variável do tipo number. Daí quando você colocar o número da cotação, 
+no qual a variavel pede, ele vai te dar um retorno da maltiplicação da cotação com o valor em dolar mais a string em 
+reais (R$), daí quando você digitar o valor ele vai multiplicar por 100, daí ele vai imprimir tal resultado.*/
+
+/****************** EXERCICIO 2 (INTERPRETAÇÃO) *********************/
+
+/* tem uma função em que nela tem duas coisas para resolver, o tipo de intestimento e o valor, e com isso é utilizado
+ o switch,  a estrutura condicional  e nele vao ser executados quatro casos diferentes blocos de código de acordo 
+ com  as diferentes condições (a poupança, nele você digita o valor e esse valor vai ser multiplicado por 1.03 e assim 
+ também acontece com os outros, Renda fixa, CDB e ações), daí ele vai te retornar o valor apos investimento,caso 
+ não digite nada, ele vai te dar um alert. Depois vem o  parametro e nela tem duas variaveis que vão chamar as 
+ coisas que tão no switch e vai te devolver uma string junto com o valor, sendo assim imprimindo eles */
+
+ /**************** EXERCICIO 3 (INTERPRETAÇÃO) **********************/
+
+/* tem uma variavel com uma lista de numeros, abaixo dela tem dois arrays vazios. abaixo tem o for que vai iniciar 
+uma variavel que vai ser uma condicional "if" se um numero for divisivel por dois e igual a 0, ele vai receber um push
+e entrar na lista do primeiro array. "Else" caso não for par ele vai entrar pro sengundo array. Depois ele vai imprimir 
+um string junto com a qauntidade de numeros total. depois ele vai imprimir apenas a quantidade de numeros que tem no 
+array 1 e depois o mesmo acontecerá com o array 2.*/
+
+/****************** EXERCICIO 4 (INTERPRETAÇÃO) *************************/
+
+/* tem a variavel que contem uma lista de numeros. Se o numero for menor que o infinito, ele irá subtrair e  imprimir os valores  negativos, caso seja 
+maior ele irá somar e imprimir os valores normais (sem estar no negativo)            */
+
+
+/********************* EXERCICIO 1 (LOGICA DE PROGRAMAÇÃO) ***********************/
+
+/* 3 formas de percorrer listas: FOR; WHILE; ForEach*/
